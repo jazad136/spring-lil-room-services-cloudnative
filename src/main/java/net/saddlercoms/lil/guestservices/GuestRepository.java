cb374e8862a2c1078b2b1601e0f4e5e3e54e23c7
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-04-13 21:26:59 -0400

    Finish Basic Rest Controller Services

---
+++ src/main/java/net/saddlercoms/lil/guestservices/GuestRepository.java
@@ -0,0 +1,8 @@
+package net.saddlercoms.lil.guestservices;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface GuestRepository extends CrudRepository<Guest, Long>{
+
+}
---
+++ src/test/java/net/saddlercoms/lil/guestservices/GuestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.saddlercoms.lil.guestservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
